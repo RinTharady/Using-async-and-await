6929be28a0ccb5e639791ec768d8da4c8ae92b57
RinTharady <Rintharady23@gmail.com>
2025-01-18 09:46:00 +0700

    Update Using async and await.js

--- Using async and await.js
+++ Using async and await.js
@@ -1,13 +1,13 @@
 function fetchData(): Promise<unknown> {
   return new Promise((resolve, reject): void => {
-    // Replace 'Sok Dara' with your full name
+    
     setTimeout(() => resolve('Data fetched! Student Name: Rin Tharady'), 1000); 
   });
 }
 
 async function fetchAndProcessData(): Promise<void> {
   try {
-    // Replace 'Sok Dara' with your full name
+  
     console.log('Student Name: Rin Tharady'); 
     console.log('Using async/await:');
     const data = await fetchData();
